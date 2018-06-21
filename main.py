37be4122ad912e866f4ca3bfb16d26e59ea3a25a
woodbury shortridge <whshortridge@gmail.com>
2018-06-21 17:38:18 -0400

    open close dummy logic

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ app.config['BASIC_AUTH_PASSWORD'] = 'chicken'
 
 basic_auth = BasicAuth(app)
 
+gate = "down"
+
 @app.route("/")
 @basic_auth.required
 def hello():
@@ -15,9 +17,42 @@ def hello():
    timeString = now.strftime("%Y-%m-%d %H:%M")
    templateData = {
       'title' : 'HELLO BILL!',
-      'time': timeString
+      'time': timeString,
+      'gate' : gate
       }
    return render_template('main.html', **templateData)
+
+@app.route("/gate/<action>")
+@basic_auth.required
+def action(action):
+
+   global gate
+
+   if action == "up":
+      print (action, "Let the chickens loose!")
+      message = "Chickens are loose!"
+      gate = "up"
+
+   elif action == "down":
+      print (action, "close the hatch")
+      message = "The coop is shut!"
+      gate = "down"
+
+   else:
+      print ("hacker alert!")
+      message = "You can't do that!"
+
+   now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+
+   templateData = {
+      'title' : message,
+      'time': timeString,
+      'gate' : gate
+   }
+
+   return render_template('main.html', **templateData)
+
 
 if __name__ == "__main__":
    app.run(host='localhost', port=3000, debug=True)
